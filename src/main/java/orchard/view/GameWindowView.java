cd47888b12a7a00eff64478eb1c2aa35a8ea9df2
Sofia Aylal <sofia.aylal@etu.unilim.fr>
2023-05-19 16:17:23 +0200

    add getGameScene and getStartGameBtn methods

--- src/main/java/orchard/view/GameWindowView.java
+++ src/main/java/orchard/view/GameWindowView.java
@@ -1,14 +1,17 @@
 package orchard.view;
 
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
+import orchard.model.Board;
 import orchard.model.Die;
 
 public class GameWindowView {
@@ -30,6 +33,22 @@ public class GameWindowView {
         return new BackgroundImage(image,
                 BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
                   BackgroundSize.DEFAULT);
+    }
+    
+    public Scene getGameScene(DieWindowView dieFX){
+        BackgroundImage background = getGameBackground(new Image("/gameBackground.png", 910, 682, false, true));
+        
+        BorderPane root = new BorderPane();
+        
+        Board boardModel = new Board();
+        root.setBottom(borderPaneBottomCreation(boardModel.die(), dieFX));
+        root.setBackground(new Background(background));
+        root.setCenter(BoardView.gridPaneTreesCreation(boardModel.trees())); 
+        return new Scene(root, 910, 682);
+    }
+
+    public Button getStartGameBtn() {
+        return this.startGameBtn;
     }
     
 }
