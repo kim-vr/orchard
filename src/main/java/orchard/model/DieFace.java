29951793d55decfe3b22bf0b120ffa57bc35c08b
Kim van Rijswijk <kim.van@etu.unilim.fr>
2023-05-19 16:33:37 +0200

    add associatedSymbol argument in DieFace enumeration

--- src/main/java/orchard/model/DieFace.java
+++ src/main/java/orchard/model/DieFace.java
@@ -1,5 +1,19 @@
 package orchard.model;
 
 public enum DieFace {
-	RED,YELLOW,BLUE,GREEN;	
+	RED(Fruit.CHERRY),
+	YELLOW(Fruit.PEAR),
+	BLUE(Fruit.PLUM),
+	GREEN(Fruit.APPLE);
+
+	private Fruit associatedSymbol;
+
+	DieFace(Fruit associatedSymbol) {
+		this.associatedSymbol = associatedSymbol;
+	}
+
+	public Fruit getAssociatedSymbol() {
+		return associatedSymbol;
+	}
+	
 }
