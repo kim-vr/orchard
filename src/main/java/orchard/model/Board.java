6836a8bb162812273aea0956dbd5b7eb05f166ae
Sofia Aylal <sofia.aylal@etu.unilim.fr>
2023-05-19 11:56:45 +0200

    add Die, numberOfRounds and treesInitialisation methods in Board class

--- src/main/java/orchard/model/Board.java
+++ src/main/java/orchard/model/Board.java
@@ -1,5 +1,6 @@
 package orchard.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
@@ -8,6 +9,7 @@ public class Board {
     private int numberOfRounds;
     
     public Board() {
+    	this.treesInitialisation();
         this.trees = trees();
         this.die = new Die();
         this.numberOfRounds = 0;
@@ -15,6 +17,22 @@ public class Board {
     
     public List<Tree> trees(){
         return this.trees;
+    }
+    
+    public Die die() {
+        return this.die;
+    }
+    
+    public int numberOfRounds() {
+        return numberOfRounds;
+    }
+    
+    public void treesInitialisation() {
+        this.trees = new ArrayList<>(4);
+        this.trees.add(new Tree(Fruit.CHERRY));
+        this.trees.add(new Tree(Fruit.PEAR));
+        this.trees.add(new Tree(Fruit.PLUM));
+        this.trees.add(new Tree(Fruit.APPLE));
     }
     
 }
