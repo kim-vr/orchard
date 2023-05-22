8e9da3dc8808b7a28ddcb46651d17f5fff7b0fa6
Sofia Aylal <sofia.aylal@etu.unilim.fr>
2023-05-22 17:03:39 +0200

    add DieTest class with tests

--- src/test/java/orchard/DieTest.java
+++ src/test/java/orchard/DieTest.java
@@ -1,5 +1,31 @@
 package orchard;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import orchard.model.Die;
+import orchard.model.DieFace;
+
 public class DieTest {
 
+	@Test
+    public void testInitialCurrentFace() {
+        Die die = new Die();
+        assertEquals(DieFace.RED, die.currentFace());
+    }
+
+    @Test
+    public void testSetCurrentFace() {
+        Die die = new Die();
+        die.setCurrentFace(DieFace.BLUE);
+        assertEquals(DieFace.BLUE, die.currentFace());
+    }
+
+    @Test
+    public void testRollDie() {
+        Die die = new Die();
+        die.rollDie();
+        DieFace rolledFace = die.currentFace();
+        assertTrue(die.faces().contains(rolledFace));
+    }
 }
